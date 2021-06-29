5ce2854ef2dfe063aad86ed8837b8d5a8c1b0b35
AO Industries, Inc <support@aoindustries.com>
2021-06-29 02:50:07 -0500

    Now supports Java 9+ modules.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,33 @@
+/*
+ * noc-monitor-portmon - Port monitoring implementations.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of noc-monitor-portmon.
+ *
+ * noc-monitor-portmon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * noc-monitor-portmon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with noc-monitor-portmon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module com.aoindustries.noc.monitor.portmon {
+	exports com.aoindustries.noc.monitor.portmon;
+	// Direct
+	requires com.aoapps.hodgepodge; // <groupId>com.aoapps</groupId><artifactId>ao-hodgepodge</artifactId>
+	requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
+	requires com.aoapps.net.types; // <groupId>com.aoapps</groupId><artifactId>ao-net-types</artifactId>
+	requires com.aoindustries.aoserv.client; // <groupId>com.aoindustries</groupId><artifactId>aoserv-client</artifactId>
+	// Java SE
+	requires java.logging;
+	requires java.sql;
+}
